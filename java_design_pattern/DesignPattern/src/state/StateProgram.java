289acec6339dad8a87c21750612e0d52982147ce
khmdev87 <khmdev87@google.com>
2022-08-20 23:27:31 +0900

    Changes to be committed: 	new file:   java_design_pattern/DesignPattern/src/state/ModeState.java 	new file:   java_design_pattern/DesignPattern/src/state/ModeSwitch.java 	new file:   java_design_pattern/DesignPattern/src/state/README.md 	new file:   java_design_pattern/DesignPattern/src/state/StateProgram.java

---
+++ java_design_pattern/DesignPattern/src/state/StateProgram.java
@@ -0,0 +1,12 @@
+package state;
+
+public class StateProgram {
+  public static void main(final String[] args) {
+    final ModeSwitch modeSwitch = new ModeSwitch();
+
+    modeSwitch.onSwitch(); // "FROM LIGHT TO DARK" 출력
+    modeSwitch.onSwitch(); // "FROM DARK TO LIGHT" 출력
+    modeSwitch.onSwitch(); // "FROM LIGHT TO DARK" 출력
+    modeSwitch.onSwitch(); // "FROM DARK TO LIGHT" 출력
+  }
+}
